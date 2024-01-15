4bcdc6d832061072d8601dcad02ae062b2787a8c
gisbseb <gisbseb@gmail.com>
2024-01-15 10:57:12 +0100

    test

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,23 @@
+const Card = () => {
+  const test = {
+    id: "39",
+    gender: "male",
+    firstname: "Nino",
+    lastname: "Perrin",
+    email: "nino.perrin@example.com",
+    password: "$2b$10$ma2la7PRWHmZudZ4yrGssurGefnfsgZhTzCwtQCU4TD24JMPw7Lge",
+    phone: "03-29-65-34-00",
+    birthdate: "1992-02-13",
+    city: "Saint-étienne",
+    country: "France",
+    photo: "https://randomuser.me/api/portraits/men/45.jpg",
+    category: "Marketing",
+    isAdmin: false,
+  };
+
+  return (
+    <div className="card">
+      <h3></h3>
+    </div>
+  );
+};
--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(() => {
   console.log("Server is running on port 3000");
 });
