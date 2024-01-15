512040ed1a4f0fda649d316bf82d481c08b4fe9a
gisbseb <gisbseb@gmail.com>
2024-01-15 11:47:49 +0100

    tt

--- server/app.js
+++ server/app.js
@@ -5,9 +5,7 @@ import dbConnect from "./config/dbConnect.js";
 dotenv.config();
 const app = express();
 
-<<<<<<<<< Temporary merge branch 1
 dbConnect();
 app.listen(3000, () => {
-
   console.log("Server is running on port 3000");
 });
