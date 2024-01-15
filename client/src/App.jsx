ebe329125ff7c66ea1ec6670915d33dcd2d4a11a
gisbseb <gisbseb@gmail.com>
2024-01-15 11:40:45 +0100

    tt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
+import Collaborateurs from "./features/Homepage/Collaborateurs/Collaborateurs.jsx";
 
 function App() {
   return (
@@ -8,12 +9,9 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<p>Login</p>} />
         <Route path="home" element={<p>HOMEPAGE - ICI GET RANDOM USER</p>} />
-        <Route
-          path="collaborateurs"
-          element={<p>collaborateurs - ICI GET ALL USER</p>}
-        />
+        <Route path="collaborateurs" element={<Collaborateurs />} />
         <Route path="compte" element={<p>Profil - Get user/id</p>}>
-          <Route path="update" element={<p>Update user</p>} />
+          <Route path="update/:id" element={<p>Update user</p>} />
         </Route>
       </Route>
     </Routes>
---
+++ client/src/utils/url.js
@@ -0,0 +1,3 @@
+const BASE_URL = "http://localhost:9000/api";
+export const COLLABORATEURS_URL = BASE_URL + "/getAllUsers";
+export const SINGLE_COLLAB = BASE_URL + "";
