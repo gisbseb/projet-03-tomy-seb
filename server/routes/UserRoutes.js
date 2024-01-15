94da763b9af9bea3b4f1aae9b8b559ad5e002ac7
yuzurhia <phalyvong.tomy@gmail.com>
2024-01-15 15:29:57 +0100

    update, delete user

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -3,14 +3,17 @@ import {
   getAllUsers,
   register,
   login,
+  deleteUser,
+  updateUser,
 } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 // router.get("/", getRandomUser);
 router.get("/getAllUsers", getAllUsers);
-// router.post("/", updateUser);
 router.post("/register", register);
 router.post("/login", login);
+router.delete("/delete/:id", deleteUser);
+router.post("/updateUser", updateUser);
 
 export default router;
